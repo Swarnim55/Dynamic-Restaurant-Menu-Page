83fd01f5d13ef8bf24734468d4b525dc7866d16f
Your Name Swarnim 55 <swarnimpaudyal55@gmail.com>
2022-12-29 18:43:06 +0545

    First Upload

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,67 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/R.jpeg",
+    name: "Cheese Sandwhich",
+    category: "Breakfast",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the grilled Cheese Sandwich. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 2,
+    image: "images/burger.jpeg",
+    name: "Cheese Burger",
+    category: "Breakfast",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the grilled Cheese Burger. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 3,
+    image: "images/pasta.jpeg",
+    name: "Cheese Pasta",
+    category: "Breakfast",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the Cheese Pasta. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 4,
+    image: "images/fried_chick.jpeg",
+    name: "Fried Chicken",
+    category: "Snacks",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the Cheese Pasta. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 5,
+    image: "images/spaghetti.jpeg",
+    name: "Spaghetti",
+    category: "Lunch",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the Cheese Pasta. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 6,
+    image: "images/dumpling.jpeg",
+    name: "Steamed Dumplings",
+    category: "Lunch",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the Cheese Pasta. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+  {
+    id: 7,
+    image: "images/dumpling.jpeg",
+    name: "Steamed Dumplings",
+    category: "Dinner",
+    price: 50,
+    description:
+      "Feel the cheesy delightful taste of the Cheese Pasta. Lorem ipsum, dolor sit amet consectetur adipisicing elit.   Architecto, voluptates nulla. Rem possimus suscipit tempore!",
+  },
+];
+
+export default Menu;
